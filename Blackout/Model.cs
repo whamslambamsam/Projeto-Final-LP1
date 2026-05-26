920cc029d06b2896e7f9359fdd0d2f8ea3b3f37e
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-26 17:35:38 +0100

    Barebones template

---
+++ Blackout/Model.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using Spectre.Console;
+
+namespace Blackout
+{
+    public class Model
+    {
+    
+    }
+}
