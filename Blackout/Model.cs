69b720f8e8fd93a85d487236ee5d111c021ddb3a
Miguel Martins <miguelarbmartins@gmail.com>
2026-05-27 16:38:29 +0100

    Documentação XML em Model.cs

--- Blackout/Model.cs
+++ Blackout/Model.cs
@@ -9,11 +9,25 @@ namespace Blackout
 {
     public class Model
     {
+        /// <summary>
+        /// Metodo usado para criar a grid
+        /// dependendo da dificuldade que o jogador selecionou.
+        /// </summary>
+        /// <param name="length">
+        /// Largura total da grid.
+        /// </param>
+        /// <param name="width">
+        /// Altura total da grid.
+        /// </param>
+        /// <returns>
+        /// As dimenções da grid criada para o jogador.
+        /// </returns>
+        /// <remarks>
+        /// Foi usado AI para perceber como é que poderi-se fazer as grids.
+        /// </remarks>
         public bool[,] GridSize(int length, int width)
         {
-            bool[,] grid = new bool[length, width]; // IA para saber como é que
-                                                    // grids são feitas.
-            
+            bool[,] grid = new bool[length, width];
             return grid;
         }
     }
